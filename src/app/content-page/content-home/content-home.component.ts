106ed7fc989e8f284b64f2b6ffcf2caa118cb2f2
Leo Kim <bhplus.kim@outlook.com>
2024-11-09 16:15:23 -0500

    code clean up

--- src/app/content-page/content-home/content-home.component.ts
+++ src/app/content-page/content-home/content-home.component.ts
@@ -1,17 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { Component, inject } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { WordModalService } from '../../services/word-modal/word-modal.service';
-import { ToolbarComponent } from '../../shared/toolbar/toolbar.component';
-import { sampleWordMeanings } from '../../data-models/sample-word-meaning';
-import { WordMeaningData } from '../../data-models/word-meaning.model';
-import { WordListItemComponent } from '../word-list-item/word-list-item.component';
 import { Store } from '@ngrx/store';
-import { selectWordList } from '../../states/word.selectors';
+import { WordMeaningData } from '../../data-models/word-meaning.model';
+import { WordModalService } from '../../services/word-modal/word-modal.service';
 import { decreaseWordDifficulty, increaseWordDifficulty, removeWord } from '../../states/word.actions';
+import { selectWordList } from '../../states/word.selectors';
+import { WordListItemComponent } from '../word-list-item/word-list-item.component';
 
 @Component({
   selector: 'app-content-home',
@@ -20,25 +15,17 @@ import { decreaseWordDifficulty, increaseWordDifficulty, removeWord } from '../.
   styleUrl: 'content-home.component.scss',
   imports: [
     CommonModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatIconModule,
     MatListModule,
     WordListItemComponent,
-    ToolbarComponent,
   ],
 })
-export class ContentHomeComponent implements OnInit {
+export class ContentHomeComponent {
 
   readonly wordModalService = inject(WordModalService);
 
   wordList$ = this.store.select(selectWordList);
 
   constructor(private store: Store) { }
-
-  ngOnInit(): void {
-    // this.restoreList();
-  }
 
   onUpdate(data: WordMeaningData, index: number) {
     this.wordModalService.openUpdate(data, index);
